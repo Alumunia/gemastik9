abab7e7eb2ff20a28f0d7b2ee84e9cb59dfc7b8d
RIZKI ADI UTOMO <alumunia@gmail.com>
2016-08-20 03:34:14 +0700

    20-08-2016

---
+++ framework/app/Http/Controllers/memberController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+
+class memberController extends Controller {
+
+    public function __construct() {
+        
+    }
+
+    public function index() {
+        
+    }
+
+    public function getTahap1() {
+        return view('tahap1Form', array(
+            'title'=>'Tahap 1'
+        ))
+        
+    }
+
+    public function postTahap1() {
+        
+    }
+
+    public function getTahap2() {
+        
+    }
+
+    public function postTahap2() {
+        
+    }
+
+    public function getTahap3() {
+        
+    }
+
+    public function postTahap3() {
+        
+    }
+
+}
--- framework/app/Http/routes.php
+++ framework/app/Http/routes.php
@@ -18,3 +18,20 @@ Route::get('/', function () {
 Route::get('/myName', 'homeController@index');
 Route::get('/register', 'homeController@getRegister');
 Route::post('/postRegister', 'homeController@postRegister');
+
+
+
+
+// Tahap 1
+Route::get('/tahap-1', 'memberController@getTahap1');
+Route::post('/tahap-1', 'memberController@postTahap1');
+
+// Tahap 2
+Route::get('/tahap-2', 'memberController@getTahap2');
+Route::post('/tahap-2', 'memberController@postTahap2');
+
+// Tahap 3
+Route::get('/tahap-3', 'memberController@getTahap3');
+Route::post('/tahap-3', 'memberController@postTahap3');
+
+
